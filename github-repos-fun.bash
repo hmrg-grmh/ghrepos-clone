ba6224965ebcdc90afe864c5970f7dc1c75ec0b9
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-12-03 15:38:28 +0800

    Create github-repos-fun.bash
    add single ability

--- github-repos-fun.bash
+++ github-repos-fun.bash
@@ -2,7 +2,7 @@ ghrepos-clone ()
 {
     `# para`
     
-    u="${1:-firecracker-microvm}" \
+    n="${1:-firecracker-microvm}" \
     p="${2:-1}" \
     per="${3:-100}" \
     proxy="${4:-https://ghproxy.com/https://github.com}" \
@@ -12,67 +12,75 @@ ghrepos-clone ()
     
     
     `# doc`
-
+    
     (($# != 0)) ||
     {
         cat <<HELPDOC ;
 
 Required Commands:
-
-- bash
-- tee
-- curl
-- jq
-- xargs
-- git
+  bash tee curl jq xargs git
 
 
 Usage:
-
-  CMD           \$1                 \$2           \$3                        \$4      \$5
-  -------       ------             -----        -----                     -----   -----
-  ghrepos-clone <github-user-name> [which page] [how much repos per page] [proxy] [type]
+  
+  CMD           \$1        \$2         \$3         \$4      \$5
+  -------       ------    -----      -----      -----   -----
+  ghrepos-clone <gh-name> [page num] [page len] [proxy] [type]
   
   
   default value is:
     
-    ______                     ______
-    which page :               ${p:-<nil>}
-    ______                     ______
-    how much repos per page :  ${per:-<nil>}
-    ______                     ______
-    proxy :                    ${proxy:-<nil>}
-    ______                     ______
-    type :                     ${type:-<nil>}
-  
+    ______                 ______
+    page num :             ${p:-<nil>}
+    ______                 ______
+    page len :             ${per:-<nil>}
+    ______                 ______
+    proxy :                ${proxy:-<nil>}
+    ______                 ______
+    type :                 ${type:-<nil>}
+    
   
   demo
+  
+  - if you wans to gh-clone repos 
+    like \`$n/*\`, you can just 
+    run this :
+  
+    ghrepos-clone $n
     
-  - if you wans to gh-clone repos like \`$u/*\`, you can just run this :
+  
+  - or if you wans to 
+    use page 2, you can 
+    run this :
+  
+    ghrepos-clone $n 2
     
-    ghrepos-clone $u
+  
+  - or you just wans to 
+    change the proxy maybe 
+    as \`https://gghhhub.iioo\`, you can 
+    run this :
+  
+    ghrepos-clone $n '' '' https://gghhhub.iioo
     
+  
+  - or if you just wans to 
+    download the wiki :
+  
+    ghrepos-clone $n '' '' '' wiki
     
-  - or if you wans to use page 2, you can run this :
-    
-    ghrepos-clone $u 2
-    
-    
-  - or you just wans to change the proxy maybe as \`https://gghhhub.iioo\`, you can run this :
-    
-    ghrepos-clone $u '' '' https://gghhhub.iioo
-    
-    
-  - or if you just wans to download the wiki :
-    
-    ghrepos-clone $u '' '' '' wiki
-    
-    
+  
+  - clone single repo 
+    such as \`zettlr/zettlr\` :
+
+    ghrepos-clone zettlr/zettlr
+
+  
   
   And if you need flag like \`--depth\` for \`git clone\` command ,
   You may need to change the codes under '\`# code\`' in these codes .
   
-  
+
 %%%% doc here over %%%%
 
 enjoy gh! :)
@@ -86,13 +94,30 @@ HELPDOC
     
     `# code`
     
-    curl https://api.github.com/users/"$u"/repos?per_page="$per"\&page="$p" |
-        jq '.[]|.full_name' |
-        xargs -i -- echo "{}${type:+.$type}" |
-        tee -a -- /dev/stderr |
-        xargs -i -P1 -- bash -c '
-            git clone -q -- '"'$proxy'"'/{}.git {} &&
-            { echo :ok :gh :: {} ... "$(date +%FT%T.%3N%:::z)" ; } ||
-            { echo :err :gh :: {} ; }' |
-        tee gh-cloning-repos."$u".log ;
+    repos_ ()
+    {
+        curl https://api.github.com/users/"$n"/repos?per_page="$per"\&page="$p" |
+            jq '.[]|.full_name' |
+            xargs -i -- echo "{}${type:+.$type}" |
+            tee -a -- /dev/stderr |
+            xargs -i -P1 -- bash -c '
+                git clone -q -- '"'$proxy'"'/{}.git {} &&
+                { echo :ok :gh :: {} ... "$(date +%FT%T.%3N%:::z)" ; } ||
+                { echo :err :gh :: {} ; }' |
+            tee gh-cloning-repos."$n".log ;
+    } ;
+    
+    repo_ ()
+    {
+        git clone -- "$proxy"/"$n".git "$n" &&
+        { echo :o :gh :: "$n" .. "$(date +%FT%T.%3N%:::z)" ; } ||
+        { echo :e :gh :: "$n" ; } |
+            tee -a gh-cloning-repos."$(dirname "$n")".log ;
+    } ;
+
+    `# run`
+
+    (("$(expr length "$(dirname "$n")")" != 0)) &&
+    { repo_ ; } || { repos_ ; } ;
 } ;
+
